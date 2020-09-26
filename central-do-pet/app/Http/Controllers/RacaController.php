716b8692a2d92a2a7ee94f83fa9bf608cebf7194
junior <cleovaldobcc@gmail.com>
2020-09-26 18:39:22 -0300

    Metodo editar RacaController.php

--- central-do-pet/app/Http/Controllers/RacaController.php
+++ central-do-pet/app/Http/Controllers/RacaController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\doenca;
 use Illuminate\Http\Request;
 
 class RacaController extends Controller
 {
-    //
+    public function editar(Request $request){
+        $raca = doenca::find($request->id);
+        return view(' /editarDoencas', ['raca' => $raca]);
+    }
 }
