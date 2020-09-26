4af1a5d9a613ae54744ae69c6a95bf818f355930
felipebattist <felipe75655@outlook.com.br>
2020-09-25 23:34:48 -0300

    Routes de Pet

--- central-do-pet/routes/web.php
+++ central-do-pet/routes/web.php
@@ -33,6 +33,15 @@ Route::get('/atualizar/doencas', '\App\Http\Controllers\DoencaController@atualiz
 Route::get('/adicionar/doencas', '\App\Http\Controllers\DoencaController@prepararAdicionar');
 Route::post('/adicionar/doencas', '\App\Http\Controllers\DoencaController@adicionar');
 
+#RotasDePet
+Route::post('/listar/pet', '\App\Http\Controllers\PetController@listar');
+Route::post('/adicionar/pet', '\App\Http\Controllers\PetController@prepararAdicionar');
+Route::post('/editar/pet/{id}', '\App\Http\Controllers\PetController@editar');
+Route::post('/remover/pet/{id}', '\App\Http\Controllers\PetController@remover');
+Route::post('/atualizar/pet', '\App\Http\Controllers\PetController@atualizar');
+Route::post('/adicionar/pet', '\App\Http\Controllers\PetController@adicionar');
+
+
 
 
 
