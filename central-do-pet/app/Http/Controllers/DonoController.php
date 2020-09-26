a00b1c7c692f4d4b80e363328923d0bf24f8563b
junior <cleovaldobcc@gmail.com>
2020-09-25 22:17:02 -0300

    Metodo atualizar DonoController

--- central-do-pet/app/Http/Controllers/DonoController.php
+++ central-do-pet/app/Http/Controllers/DonoController.php
@@ -9,7 +9,7 @@ class DonoController extends Controller
 {
     public function listar() {
         $donos = dono::all();
-        return view('listarDono', ['donos' => $donos]);
+        return view('listaDonos', ['donos' => $donos]);
     }
 
     public function editar(Request $request) {
@@ -17,4 +17,17 @@ class DonoController extends Controller
         return view('editarDono', ['dono' => $dono]);
     }
 
+    public function atualizar(Request $request){
+        $dono = dono::find($request->id);
+        $dono-> pet_id -> $request-> pet_id;
+        $dono-> cpf -> $request-> cpf;
+        $dono-> nome -> $request-> nome;
+        $dono-> contato -> $request-> contato;
+        $dono-> endereco -> $request-> endereco;
+        $dono-> sexo -> $request-> sexo;
+        $dono-> email -> $request-> email;
+        $dono-> data_de_nascimento -> $request-> data_de_nascimento;
+        $dono->update();
+        return redirect("/listar/donos");
+    }
 }
