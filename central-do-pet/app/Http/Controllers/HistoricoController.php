1d1270d3bdc47d18ce98c3761291c1230efa50ca
felipebattist <felipe75655@outlook.com.br>
2020-09-26 00:30:01 -0300

    atualizar historicos

--- central-do-pet/app/Http/Controllers/HistoricoController.php
+++ central-do-pet/app/Http/Controllers/HistoricoController.php
@@ -32,5 +32,13 @@ class HistoricoController extends Controller
 
     }
 
+    public function atualizar(Request $request){
+        $historico = historico::find($request->id);
+        $historico->pet_id = $request->pet_id;
+        $historico->alergias = $request->alergias;
+        $historico->update();
+        return redirect('Historico/listar/historicos');
+    }
+
 
 }
