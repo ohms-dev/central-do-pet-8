dc48651393572de585d21045c194ab1f3dab9e3c
felipebattist <felipe75655@outlook.com.br>
2020-09-26 00:05:16 -0300

    listar historicos

--- central-do-pet/app/Http/Controllers/HistoricoController.php
+++ central-do-pet/app/Http/Controllers/HistoricoController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\historico;
 use Illuminate\Http\Request;
 
 class HistoricoController extends Controller
 {
-    //
+    public function listar(){
+        $historicos = historico::all();
+        return view('listaHistoricos', ['historicos' => $historicos]);
+    }
+
+
 }
