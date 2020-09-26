319f1b3215116307cde4e903714581a186564b55
felipebattist <felipe75655@outlook.com.br>
2020-09-25 22:06:41 -0300

    Adicionar novo usuario

--- central-do-pet/app/Http/Controllers/UsuarioController.php
+++ central-do-pet/app/Http/Controllers/UsuarioController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    //
+    public function adicionar(Request $request){
+        $usuario = new \app\Models\usuario();
+        $usuario->nome = $request->nome;
+        $usuario->contato = $request->contato;
+        $usuario->endereco = &$request->endereco;
+        $usuario->senha = $request->senha;
+        $usuario->funcao = $request->funcao;
+        $usuario->email = $request->email;
+        $usuario->sexo = $request->sexo;
+        $usuario->data_de_nascimento = $request->data_de_nascimento;
+        $usuario->save();
+        return  redirect('\listar\usuarios');
+
+    }
 }
