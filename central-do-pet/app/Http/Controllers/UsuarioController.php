aa81f7e748c75fb53b4406f01bfb52e3445beca6
felipebattist <felipe75655@outlook.com.br>
2020-09-25 22:25:41 -0300

    Atualizar Usuarios

--- central-do-pet/app/Http/Controllers/UsuarioController.php
+++ central-do-pet/app/Http/Controllers/UsuarioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\usuario;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller{
@@ -34,6 +35,22 @@ class UsuarioController extends Controller{
     public function editar(Request $request){
         $usuario = \app\Models\usuario::find($request->id);
         return view('editarUsuario', ['usuario' => $usuario]);
+    }
+
+    public function atualizar(Request &$request){
+        $usuario = \app\Models\usuario::find($request->id);
+        $usuario->nome = $request->nome;
+        $usuario->contato = $request->contato;
+        $usuario->endereco = &$request->endereco;
+        $usuario->senha = $request->senha;
+        $usuario->funcao = $request->funcao;
+        $usuario->email = $request->email;
+        $usuario->sexo = $request->sexo;
+        $usuario->data_de_nascimento = $request->data_de_nascimento;
+        $usuario->update();
+        return redirect('/listar/usuarios');
+
+
     }
 
 
