6b0d462afc9ec31209f7080088f9e343adc67a57
junior <cleovaldobcc@gmail.com>
2020-09-26 00:36:26 -0300

    retiradas minhas routes de dono

--- central-do-pet/routes/web.php
+++ central-do-pet/routes/web.php
@@ -12,15 +12,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
-    return view('teste');
-});
-
-
 
 
 
