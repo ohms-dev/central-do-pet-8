12e183562fca8037281c05d78fcfb8bfa6ad767d
Arnaldo Vitor <arnaldovitorbarros@gmail.com>
2020-09-28 15:11:27 -0300

    Validator de doença

---
+++ central-do-pet/app/Validator/doencaValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Validator;
+use Illuminate\Support\Facades\Validator;
+
+class doencaValidator extends \Exception
+{
+    public function validate($data) {
+        $validator = Validator::make($data, \App\Models\doenca::$rules, \App\Models\doenca::$messages);
+        if(!$validator->errors()->isEmpty())
+            throw new ValidationException($validator, "Erro na validação de doença!");
+        return $validator;
+
+    }
+}
--- central-do-pet/database/seeders/HistoricoSeeder.php
+++ central-do-pet/database/seeders/HistoricoSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class HistoricoSeeder extends Seeder
 {
