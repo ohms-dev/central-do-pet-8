76810b6f59581f809a36167f3f507c6a4e77188c
junior <cleovaldobcc@gmail.com>
2020-09-28 14:49:09 -0300

    valicação de transfusao funcionando

--- central-do-pet/resources/views/Transfusao/adicionarTransfusao.blade.php
+++ central-do-pet/resources/views/Transfusao/adicionarTransfusao.blade.php
@@ -12,6 +12,15 @@
     Data: <input type="date" name="data" />
     <br>
     <input type="submit" value="cadastrar" />
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <script type='text/javascript'>alert('{{ $error }}');</script>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </form>
 </body>
 </html>
