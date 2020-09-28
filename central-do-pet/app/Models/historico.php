ea81659a57a3878363afb9fb4f73a0bc05086c6d
junior <cleovaldobcc@gmail.com>
2020-09-28 00:28:44 -0300

    valicação de historico funcionando (incompleto)

--- central-do-pet/app/Models/historico.php
+++ central-do-pet/app/Models/historico.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class historico extends Model
-{
-    use HasFactory;
+
+class historico extends Model implements Authenticatable{
+
+    protected $fillable = ['pet_id', 'alergias'];
+
+    public static $rules = ['pet_id'=>'required'];
+
+    public static $messages = ['pet_id.*' => 'O campo Pet Id é obrigatório'];
 
     public function Pet() {
       return $this->belongsTo('central-do-pet\Pet');
@@ -23,5 +29,36 @@ class historico extends Model
 
     public function vacinas(){
       return $this->hasMany('central-do-pet\Vacina');
+    }
+
+    public function getAuthIdentifierName()
+    {
+        // TODO: Implement getAuthIdentifierName() method.
+    }
+
+    public function getAuthIdentifier()
+    {
+        // TODO: Implement getAuthIdentifier() method.
+    }
+
+    public function getAuthPassword()
+    {
+        // TODO: Implement getAuthPassword() method.
+    }
+
+    public function getRememberToken()
+    {
+        // TODO: Implement getRememberToken() method.
+    }
+
+    public function setRememberToken($value)
+    {
+        // TODO: Implement setRememberToken() method.
+    }
+
+    public function getRememberTokenName()
+    {
+
+        // TODO: Implement getRememberTokenName() method.
     }
 }
--- central-do-pet/resources/views/Historico/adicionarHistorico.blade.php
+++ central-do-pet/resources/views/Historico/adicionarHistorico.blade.php
@@ -6,12 +6,23 @@
   <body>
     <h1>Cadastrar Historico</h1>
     <form action="/adicionar/historico" method="post">
-      {{ csrf_field() }}
-      Pet Id: <input type="text" name="pet_id" />
-      <br>
-      Alergias: <input type="text" name="alergias" />
-      <br>
-      <input type="submit" value="cadastrar" />
+        {{ csrf_field() }}
+        Pet Id: <input type="text" name="pet_id" />
+        <br>
+        Alergias: <input type="text" name="alergias" />
+        <br>
+        <input type="submit" value="cadastrar" />
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <script type='text/javascript'>alert('{{ $error }}');</script>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </form>
   </body>
 </html>
+
+
