1cfcaf7ca168fa871cd11d42d9b441b02e23461a
VitorAlan <vitoralan.bs@gmail.com>
2020-09-28 15:22:35 -0300

    Validação Dono, Usuário

--- central-do-pet/database/seeders/HistoricoSeeder.php
+++ central-do-pet/database/seeders/HistoricoSeeder.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use phpDocumentor\Reflection\Types\Boolean;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class HistoricoSeeder extends Seeder
 {
