2e2b50e8e1c94477360c60f352a9cc54b276f594
Arnaldo Vitor <arnaldovitorbarros@gmail.com>
2020-10-03 18:18:04 -0300

    Mudança em autorização para visualização de pets adotados ou não

--- central-do-pet/resources/views/Pet/listaPets.blade.php
+++ central-do-pet/resources/views/Pet/listaPets.blade.php
@@ -4,22 +4,41 @@
     <title>Hello</title>
   </head>
   <body>
-  <h1>Listar Pets</h1>
+  <h1>Listar Pets </h1>
+  @auth
     <table border='1'>
       @foreach ($pets as $pet)
         <tr>
           <td><a href=' /view/pet/{{$pet->id}}'>{{$pet->id}}</a></td>
           <td>{{ $pet->rga }}</td>
           <td>{{ $pet->nome }}</td>
-          @if($auth ?? '')
             <td>
+                <a href='/view/pet/{{ $pet->id }}'>Visualizar</a>
+
                 <a href='/editar/pet/{{ $pet->id }}'>Editar</a>
                 <a href='/remover/pet/{{ $pet->id }}'>Remover</a>
             </td>
-          @endif
+
         </tr>
       @endforeach
     </table>
     <a href="/adicionar/pet">Inserir Nova</a>
+  @endauth
+  @guest
+  <table border='1'>
+    @foreach ($pets as $pet)
+      @if ($pet->adotado==1)
+      <tr>
+        <td>{{ $pet->rga }}</td>
+        <td>{{ $pet->nome }}</td>
+          <td>
+              <a href='/view/pet/{{ $pet->id }}'>Visualizar</a>
+          </td>
+
+      </tr>
+      @endif
+    @endforeach
+  </table>
+  @endguest
   </body>
 </html>
