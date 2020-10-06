70c28ff816a7e2f21c5a337fce0ba974394e7a66
Arnaldo Vitor <arnaldovitorbarros@gmail.com>
2020-10-06 19:03:45 -0300

    Layout de adicionar vacina

--- central-do-pet/resources/views/Vacina/adicionarVacina.blade.php
+++ central-do-pet/resources/views/Vacina/adicionarVacina.blade.php
@@ -1,19 +1,71 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-  <head>
-    <title>Hello</title>
-  <head>
-  <body>
-    <h1>Cadastrar Vacina</h1>
+<head>
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  <!-- Nav bar -->
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #E14B56;">
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home <span class="sr-only">(Página atual)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/listar/pets">Adotar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Consultar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Equipe</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ajuda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Login</a>
+        </li>
+      </ul>
+    </div>
+</nav>
+</head>
+
+<body>
+  <div class='jumbotron'>
+
+  <h1>Cadastrar vacina <img src="https://img.icons8.com/android/24/000000/syringe.png"/></h1>
+  <p class="lead">Cadastro geral de vacinas do sistema</p>
+
+  </div>
+<div class='container'>
     <form action="/adicionar/vacina" method="post">
       {{ csrf_field() }}
-      Historico id: <input type="text" name="historico_id" />
-      <br>
-      Vacina: <input type="text" name="vacina" />
-      <br>
-      Data: <input type="date" name="data" />
-      <br>
-      <input type="submit" value="cadastrar" />
+
+      <div class="form-group">
+      <label for"historico_id">ID do histórico:</label>
+      <input type="text" class="form-control" id="historico_id" />
+      </div>
+
+      <div class="form-group">
+      <label for"vacina">Vacina</label>
+      <input type="text" class="form-control" id="vacina" />
+      </div>
+
+      <div class="form-group">
+      <label for="data">Data:</label>
+      <input type="date" class="form-control" id="data" />
+      </div>
+
+      <input type="submit" class="btn btn-primary" value="Cadastrar" />
+
       @if ($errors->any())
           <div class="alert alert-danger">
               <ul>
@@ -24,5 +76,6 @@
           </div>
       @endif
     </form>
+  </div>
   </body>
 </html>
