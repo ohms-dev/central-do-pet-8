60e165b8646cf6b0aeb9cf66e7d0a08155381137
VitorAlan <vitoralan.bs@gmail.com>
2020-10-07 13:27:09 -0300

    Ajuste cadastrar pet

--- central-do-pet/app/Models/pet.php
+++ central-do-pet/app/Models/pet.php
@@ -25,8 +25,8 @@ class pet extends Model implements Authenticatable
     }
 
     use Notifiable;
-    protected $fillable = ['nome','rga','sexo','castrado','data_de_nascimento','necessidades_especiais', 'dono_id', 'adotado'] ;
-    public static $rules = ['nome'=> 'required|min:3|max:30', 'sexo'=>'required|min:5|max:5','castrado'=>'required','data_de_nascimento'=>'required'];
+    protected $fillable = ['nome','rga','sexo','castrado','data_de_nascimento','necessidades_especiais', 'dono_id', 'adotado','registro'] ;
+    public static $rules = ['nome'=> 'required|min:3|max:30', 'sexo'=>'required','castrado'=>'required','data_de_nascimento'=>'required'];
     public static $messages = ['nome.*'=>'O campo nome é obrigatório e deve ter entre 5 e 30 letras',
         'sexo.*'=>'O campo sexo é obrigatório e deve ser macho ou fêmea','castrado.*'=>'O campo castrado é obrigatório e deve ser informado se o pet foi castrado ou não',
         'data_de_nascimento.*'=> 'O campo data de nascimento é obrigatório'];
